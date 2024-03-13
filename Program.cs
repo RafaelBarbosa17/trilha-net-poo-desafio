e87df67c99a9fd7edbc845908ba34bdffdf5cb4e
RafaelBarbosa17 <rafaelvbarbosaa@gmail.com>
2024-03-13 18:20:55 -0300

    Entrega do desafio da dio sobre orientação a objetos com C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "31982844409", modelo: "n-10", imei: "44224422", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "31981024183", modelo: "14 pro max", imei: "22172217", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
